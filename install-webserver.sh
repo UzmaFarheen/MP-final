bb0791c5399cdd3d248b69b15b28d16e972d5ca6
UzmaFarheen <unln@hawk.iit.edu>
2015-11-29 17:29:09 -0600

    added a new clone statement

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo apt-get -y update
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5 php5-imagick
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick
 
 git clone https://github.com/UzmaFarheen/MP-final.git
+git clone https://github.com/UzmaFarheen/MP1-PHP.git
 
 sudo mv ./MP1-PHP/index.html /var/www/html
 sudo mv ./MP1-PHP/page2.html /var/www/html
