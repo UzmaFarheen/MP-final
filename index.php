bda883ee4144a54b7064c66523e3819604c31ea0
UzmaFarheen <unln@hawk.iit.edu>
2015-11-30 00:17:56 -0600

    reverted back to original

--- index.php
+++ index.php
@@ -25,14 +25,22 @@ label {
 <a href='gallery.php'/>View Images if Backup is in progress!</a></br>
 <a href='introspection.php'/>DB Backup!</a></br>
 </div>
-
+<?php
+if((isset($_SESSION['introspec']))&&($_SESSION['introspec'])){
+echo "MySQL dump in progress Admin has disabled form! Click on view Images to view gallery ";
+}
+else
+{
+echo 'test';
+echo (isset($_SESSION['introspec']));
+?>
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="result.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
 <label>    Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/$
-Enter Email of user: <input type="email" name="useremail"><br />
+<label>Enter Email of user: <input type="email" name="useremail"><br /></label>
 Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phoneforsms">
 
 
