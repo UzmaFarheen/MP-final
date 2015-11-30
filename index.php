36517b1c38d84c339e94b240b4f0aedb0caab644
UzmaFarheen <unln@hawk.iit.edu>
2015-11-29 23:41:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -25,15 +25,6 @@ label {
 <a href='gallery.php'/>View Images!</a></br>
 <a href='introspection.php'/>DB Backup!</a></br>
 </div>
-<?php
-if((isset($_SESSION['introspec']))&&($_SESSION['introspec'])){
-echo "Work in Progress, View Raw Images ";
-}
-else
-{
-echo 'test';
-}
-?>
 
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="result.php" method="POST">
