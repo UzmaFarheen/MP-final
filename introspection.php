dcfc8e4ac5866b331346bd742070d0c9773d40ea
UzmaFarheen <unln@hawk.iit.edu>
2015-11-30 01:34:26 -0600

    reverted back to original

--- introspection.php
+++ introspection.php
@@ -1,55 +1,49 @@
 <?php
 session_start();
 require 'vendor/autoload.php';
-$introspec=true;
-$_SESSION['introspec']=$introspec;
-echo "=========== $introspec =======";
-$rds = new Aws\Rds\RdsClient([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
+# Creating a client for the s3 bucket
+use Aws\Rds\RdsClient;
+$client = new Aws\Rds\RdsClient([
+ 'version' => 'latest',
+ 'region'  => 'us-east-1'
 ]);
-$resultrdb = $rds->describeDBInstances(array(
-    'DBInstanceIdentifier' => 'mp1-replica'
-));
-$endpointrdb = $resultrdb['DBInstances'][0]['Endpoint']['Address'];
-    echo "============\n". $endpointrdb . "================";
-$linkrdb = mysqli_connect($endpointrdb,"UzmaFarheen","UzmaFarheen","Project");
+$result = $client->describeDBInstances([
+    'DBInstanceIdentifier' => 'mp1-replica',
+]);
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+# Connecting to the database
+$link = mysqli_connect($endpoint,"UzmaFarheen","UzmaFarheen","Project") or die("Error " . mysqli_error($link));
+/* Checking the database connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-else {
-echo "Connection to RDB Success";
-}
-$backupFile = '/tmp/FinalDB'.date("Y-m-d-H-i-s").'.gz';
-$command = "mysqldump --opt -h $endpointrdb -u UzmaFarheen -p UzmaFarheen Project | gzip > $$
-exec($command);
-echo "success";
-                        $s3 = new Aws\S3\S3Client([
-                                'version' => 'latest',
-                                'region'  => 'us-east-1'
-                        ]);
-$bucket='mpuzma'.rand().'-dbdump';
-                        if(!$s3->doesBucketExist($bucket)) {
-                                $result = $s3->createBucket([
-                                        'ACL' => 'public-read',
-                                        'Bucket' => $bucket,
-                                        ]);
-  $s3->waitUntil('BucketExists', array('Bucket' => $bucket));
-    echo "$bucket Created Successfully";
-                        }
-$result = $s3->putObject([
-'ACL' => 'public-read',
-'Bucket' => $bucket,
-'Key' => $backupFile,
-'SourceFile'   => $backupFile,
-'Body' => fopen($backupFile,'r+'),
+$uploaddir = '/tmp/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$backup = uniqid("uzmaBackup",false);
+$backupfile=$uploaddir.$backup. '.' . 'sql';
+$sqlcommand="mysqldump -u UzmaFarheen -p UzmaFarheen --host=$endpoint Project > $backupfile";
+echo $sqlcommand;
+exec($sqlcommand);
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
 ]);
-$url = $result['ObjectURL'];
-echo $url;
-echo "Database backup created successfully.";
-$urlintro       = "index.php";
-   header('Location: ' . $urlintro, true);
-   die();
-$linkrdb->close();
+$bucket = uniqid("uzmabackup",false);
+# AWS PHP SDK version 3 create bucket
+$result = $s3->createBucket([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket
+]);
+$s3->waitUntil('BucketExists', array( 'Bucket'=> $bucket));
+# PHP version 3
+$result = $s3->putObject([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket,
+    'Key' => $backupfile,
+    'SourceFile' => $backupfile,
+]);  
+$sqlurl = $result['ObjectURL'];
+echo $sqlurl;
+echo "Database backup created successfully and stored in the s3 bucket.";
 ?>
