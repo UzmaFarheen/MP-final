d2bee1b536145b499f40c005706e90ca9b14302c
UzmaFarheen <unln@hawk.iit.edu>
2015-11-29 21:13:52 -0600

    added css

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 <html>
 <head><title>Test Application</title>
 <meta charset="UTF-8">
+<style type="text/css">
+
+body {
+    font-family: "Trebuchet MS", Verdana, sans-serif;
+    font-size: 16px;
+    background-color: dimgrey;
+    color: #696969;
+    padding: 3px;
+}
+
+label {
+    font-family: Georgia, serif;
+    border-bottom: 3px solid #cc9900;
+    color: #996600;
+    font-size: 30px;
+}
+</style>
 </head>
 <body>
 
@@ -10,19 +27,20 @@
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/><br />
+<label>    Send this file: <input name="userfile" type="file" accept="image/png,image/jpeg"/$
 Enter Email of user: <input type="email" name="useremail"><br />
 Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phoneforsms">
 
 
 <input type="submit" value="Send File" />
+</label>
 </form>
-
+<label>
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
-    
+
 Enter Email of user for gallery to browse: <input type="email" name="email">
 <input type="submit" value="Load Gallery" />
+</label>
 </form>
-
 </body>
 </html>
