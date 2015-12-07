6737b3228f1885059511bc503c3837096c75bbd7
UzmaFarheen <unln@hawk.iit.edu>
2015-12-06 18:47:15 -0600

    Update launch2.sh

--- launch2.sh
+++ launch2.sh
@@ -41,12 +41,12 @@ aws cloudwatch put-metric-alarm --alarm-name Reduce --alarm-description "CPU fal
 #SNS topic for image subscription
 
 SNSTOPICPICARN=(`aws sns create-topic --name snspicture`)
-aws sns set-topic-attributes --topic-arn $SNSTOPICPICARN --attribute-name Policy --attribute-value snspicture  
+aws sns set-topic-attributes --topic-arn $SNSTOPICPICARN --attribute-name DisplayName --attribute-value snspicture  
 
 #SNS topic for cloud watch subscription
 
 SNSTOPICWATCHARN=(`aws sns create-topic --name snswatch`)
-aws sns set-topic-attributes --topic-arn $SNSTOPICWATCHARN --attribute-name Policy --attribute-value snswatch
+aws sns set-topic-attributes --topic-arn $SNSTOPICWATCHARN --attribute-name DisplayName --attribute-value snswatch
 
 #Subcribe
 
